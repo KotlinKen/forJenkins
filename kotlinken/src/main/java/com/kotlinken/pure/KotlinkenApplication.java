154cd1851a2e6b2f78c641bbb3b398cf94eb91e1
hoteAetherit <hote@aetherit.io>
2020-02-03 15:00:20 +0900

    initializing code

---
+++ kotlinken/src/main/java/com/kotlinken/pure/KotlinkenApplication.java
@@ -0,0 +1,13 @@
+package com.kotlinken.pure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinkenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinkenApplication.class, args);
+	}
+
+}
---
+++ kotlinken/src/test/java/com/kotlinken/pure/KotlinkenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotlinken.pure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinkenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
